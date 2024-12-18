b2fbbb6b05fc9c7780dfe763c53d5c1f0e8a3a70
omprakash parewa <kanuparewa371@gmail.com>
2024-12-19 00:03:49 +0530

    build script add

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,12 @@ import dotenv from 'dotenv'
 import postRoutes from './routes/post.route.js'
 import {v2 as cloudinary} from 'cloudinary'; 
 import emailRoutes from './routes/email.route.js';
+import path from 'path';
 
-const app = express(); 
+
+const app = express();
+const PORT = process.env.PORT || 5000
+const __dirname = path.resolve(); 
 
 dotenv.config()
 console.log("cloudname : ", process.env.CLOUDINARY_CLOUD_NAME)
@@ -21,12 +25,15 @@ app.use(express.urlencoded({extended:true}));
 
 
 
-app.get("/",(req,res)=>{
-    res.send("Server is ready")
-})
 app.use('/api/post',postRoutes)
 app.use('/api/email',emailRoutes); 
-const PORT = process.env.PORT || 5000
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html")); 
+})
+
 
 app.listen(PORT,()=>{
     ConnectToDb();
